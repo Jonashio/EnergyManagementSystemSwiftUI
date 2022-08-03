8c97ad3eada76eb5e3cb40eb79a3eb1b86e805f6
Jonashio <jonashio.es@gmail.com>
2022-08-03 16:04:16 +0200

    Add Structure

--- EnergyManagementSystemSwiftUI/EnergyManagementSystemSwiftUIApp.swift
+++ EnergyManagementSystemSwiftUI/EnergyManagementSystemSwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct EnergyManagementSystemSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            DashboView()
         }
     }
 }
--- EnergyManagementSystemSwiftUI/Modules/Dashbo/View/DashboView.swift
+++ EnergyManagementSystemSwiftUI/Modules/Dashbo/View/DashboView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  DashboView.swift
 //  EnergyManagementSystemSwiftUI
 //
 //  Created by Jonashio on 3/8/22.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct DashboView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
@@ -16,6 +16,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        DashboView()
     }
 }
